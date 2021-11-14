430a59fd398a21c2828d3b4738fdf5309b62f53d
jennifer26 <jenniferflores0001@gmail.com>
2021-11-14 13:17:53 +0800

    acad-affair layout inside

--- storage/Academic Affairs/index.php
+++ storage/Academic Affairs/index.php
@@ -13,13 +13,13 @@
 <meta charset="utf-8">
 <title>Item</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<!--[if lt IE 9]><script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script><script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script><![endif]--> 
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
+<!--[if lt IE 9]><script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script><script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script><![endif]-->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <link rel="stylesheet" href="assets/base.css">
 <link rel="stylesheet" href="sty1.css">
 
 <link rel="shortcut icon" href="CvSU/logo-removebg.png">
-
+<script src="jquery-3.6.0.js"></script>
 <style>
 body{
   background-image: url("assets/tmp2.jpg");
@@ -43,12 +43,12 @@ body{
 </style>
 </head>
 <body>
-<?php 
+<?php
         $db = mysqli_connect('localhost', 'root', '', 'yearbook');
         $year = date("Y");
 
         if (isset($_GET['call'])) {
-        $Ys= mysqli_real_escape_string($db, $_GET['call']);  
+        $Ys= mysqli_real_escape_string($db, $_GET['call']);
 
         $sql = "SELECT * FROM tab2 WHERE year='$Ys' ORDER BY lname";
         $result = mysqli_query($db,$sql);
@@ -61,80 +61,101 @@ body{
 </div>
   <a href="http://localhost/nformat/storage.php">Storage Files</a>
   <div class="dropdown">
-    <button class="dropbtn">Dropdown 
-      <i class="fa fa-caret-down"></i>
+    <button class="dropbtn">Dropdown
+      <i class="fa fa-caret-down fa-lg"></i>
     </button>
     <div class="dropdown-content">
       <a href="#">About History</a>
       <a href="#">About Us</a>
     </div>
-  </div> 
+  </div>
   <a href="#about">About</a>
   <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
 </div>
 <br>
 <br>
 <br>
-<form style="float:left">
-  <input type="text" name="f1">
-</form>
-<br>
-<center>
-  <table id="wrapper">
-    <thead>
-    <tr>
-      <th>Image</th>
-      <th>Last Name</th>
-      <th>First Name</th>
-      <th>Middle Initial</th>
-      <th>Position</th>
-      <th>Year</th>
-    </tr>
-  </thead>
-  <tfoot>
-    <tr>
-      <th>Image</th>
-      <th>Last Name</th>
-      <th>First Name</th>
-      <th>Middle Initial</th>
-      <th>Position</th>
-      <th>Year</th>
-    </tr>
-  </tfoot>
-  <tbody>
-    <?php
-                  if (empty($rows)) {
-          echo "<tr>";
-          echo "<td colspan='6'><center>No results found.</center></td>";
-          echo "</tr>";
-          }
-          else{
-          while($row = mysqli_fetch_array($result)){
-          echo "<tr>";
-          echo "<td data-label='Image'><center>".'<img class="image-official" src="data:image/jpeg;base64,'.base64_encode($row['image1'] ).'" style="height:80px;"/>'."</center></td>";
-          echo "<td data-label='Last Name'>" . $row['lname'] . "</td>";
-          echo "<td data-label='First Name'>" . $row['fname'] . "</td>";
-          echo "<td data-label='Middle\tInitial'>" . $row['mname'] . "</td>";
-          echo "<td data-label='Position'>" . $row['position'] . "</td>";
-          echo "<td data-label='Year'>" . $row['year'] . "</td>";
-          echo "</tr>";
-        }
-      }
-          echo "</tbody>";
-          echo "</table>";
-          echo "<br>";
-        mysqli_close($db);
-        ?>
-  </tbody>
-  </table>
-</div>
-</center>
-<br>
-<br>
+  <!--- Start here --->
+  <div class="search-container">
+    <div class="topButton">
+        <button class="add-btn-reg-a add-btn-r"><i class="fas fa-user-plus"></i>Add Member</button>
+        <div class="addMember-reg-a">
+            <form action="formFunction.php" class="form" method="post"  enctype="multipart/form-data">
+                <h2>Add Member</h2>
 
-</body>
-</html>
+                <div class="inputField">
+                <p>Select Image</p>
+                <input type="file" name="f1" required>
+
+                <p>Firstname</p>
+                <input type="text" name="Fname"required><br>
+
+                <p>Middle Initial</p>
+                <input type="text" name="Mname" maxlength="4" required><br>
+
+                <p>Lastname</p>
+                <input type="text" name="Lname"required><br>
+
+                <p>Position</p>
+                <input type="text" name="position"required><br>
+
+                <p>Year</p>
+                <input type="text" name="year" maxlength="4" required><br>
+
+                <button class="button button1" type="submit" name="submit3" >ADD</button>
+                <!--<input type="submit" name="submit1" class="button button1" value="Add">-->
+                <button class="cancel-btn-reg-a button">CANCEL</button>
+              </div>
+            </form>
+        </div>
+        <div class="input-icons">
+          <input class="inp" type="text" placeholder="Search by name" name="search-text" id="search_text_affair">
+          <i class="fas fa-search"></i>
+        </div>
+    </div>
+      <br>
+      <div id="result_affair"></div>
+  </div>
+<!-- end here -->
 <script>
+$(document).ready(function(){
+      let addButtonRegA = document.querySelector('.add-btn-reg-a');
+      let addMemberRegA = document.querySelector('.addMember-reg-a');
+      let cancelButtonRegA = document.querySelector('.cancel-btn-reg-a');
+
+      addButtonRegA.addEventListener('click', function(){
+          addMemberRegA.classList.add('form-active');
+      });
+      cancelButtonRegA.addEventListener('click', function(){
+          addMemberRegA.classList.remove('form-active');
+      });
+
+      load_data();
+            function load_data(affair_query)
+            {
+              $.ajax({
+                url:"regAffairFetch.php",
+                method:"post",
+                data:{affair_query:affair_query},
+                success:function(data)
+                {
+                  $('#result_affair').html(data);
+                }
+              });
+            }
+
+            $('#search_text_affair').keyup(function(){
+              var search = $(this).val();
+              if(search != '')
+              {
+                load_data(search);
+              }
+              else
+              {
+                load_data();
+              }
+            });
+});
 function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -144,3 +165,6 @@ function myFunction() {
   }
 }
 </script>
+
+</body>
+</html>
