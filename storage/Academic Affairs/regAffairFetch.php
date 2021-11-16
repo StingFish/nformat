b0caac1f3ec49cf9fa71c9bef9ec8f43b0e977cf
jennifer26 <jenniferflores0001@gmail.com>
2021-11-16 11:24:19 +0800

    fixing table layout

--- storage/Academic Affairs/regAffairFetch.php
+++ storage/Academic Affairs/regAffairFetch.php
@@ -20,27 +20,43 @@ if(mysqli_num_rows($result) > 0)
 {
 	$output .= '
           <div>
-					<table class="table">
-          <tbody id="table">
+					<table id="wrapper">
+					<thead>
 						<tr>
 							<th>Image</th>
+							<th>Last Name</th>
 							<th>First Name</th>
 							<th>Middle Name</th>
-							<th>Last Name</th>
 							<th>Position</th>
 							<th>Year</th>
-              <th>Action</th>
-						</tr>';
+							<th>Action</th>
+						</tr>
+					</thead>
+					<tfoot>
+						<tr>
+						<th>Image</th>
+						<th>Last Name</th>
+						<th>First Name</th>
+						<th>First Name</th>
+						<th>Position</th>
+						<th>Year</th>
+						<th>Action</th>
+						</tr>
+					</tfoot>
+					<tbody>
+
+          <tbody id="table">
+					';
 	while($row = mysqli_fetch_array($result))
 	{
 		$output .= '
-			<tr class="main">
-				<td><img class="image-official" src="data:image/jpeg;base64,'.base64_encode($row["image1"]).'"/></td>
-				<td>'.$row["fname"].'</td>
-        <td>'.$row["lname"].'</td>
-				<td>'.$row["mname"].'</td>
-				<td>'.$row["position"].'</td>
-				<td>'.$row["year"].'</td>
+			<tr>
+				<td data-label="Image"><img class="image-official" src="data:image/jpeg;base64,'.base64_encode($row["image1"]).'"/></td>
+				<td data-label="First Name">'.$row["fname"].'</td>
+        <td data-label="Last Name">'.$row["lname"].'</td>
+				<td data-label="Middle/tInitial">'.$row["mname"].'</td>
+				<td data-label="Position">'.$row["position"].'</td>
+				<td data-label="Year">'.$row["year"].'</td>
         <td align="center">
                 <button class="button2 bGreen" style="border:1px solid;width:30px;">
               <a class="delbtn" style="text-decoration:none; color:white;" href ="RegFunction.php?edit='.$row["id"].'">&#9998;</a>
