02085729ade44b1d45b362b95133b44f2c1b4a9d
jennifer26 <jenniferflores0001@gmail.com>
2022-03-01 15:28:10 +0800

    Function if theres is a duplicate data for tbl_eybook table

--- storage/Employees Database/s.php
+++ storage/Employees Database/s.php
@@ -20,20 +20,27 @@ if(isset($_POST['save2'])){
 	$dep = $_POST['pos'];
 	$year = $_POST['yr'];
 
-	$check = "SELECT * FROM folder2 WHERE type = 'A' AND year = '$year'";
-	$get = mysqli_query($mysqli, $check);
-	$fetch = mysqli_fetch_assoc($get);
-	if (!$fetch) {
-		echo "<script>alert('Create a School Year for this batch in Employees Yearbook Database.');window.location='index.php';</script>";
-	}
-	else{
-	$my = "INSERT INTO tbl_eybook (eid, elname, efname, emname, work_status, department, employee_year) VALUES ('$id', '$ln', '$fn', '$mn', '$role', '$dep', '$year')";
-	$result = mysqli_query($mysqli, $my);
-	echo mysqli_error($mysqli);
+  $queryfilter = "SELECT * FROM tbl_eybook WHERE eid='$id' AND employee_year = '$year'";
+  $query = mysqli_query($mysqli, $queryfilter) or die(mysqli_error($mysqli));
+  $fetchRow = mysqli_num_rows($query);
 
-	echo "<script>alert('Added to Yearbook Successfully.');window.location='index.php';</script>";
+  if($fetchRow >= 1){
+    echo "<script>alert('Unable to add meron na sya sa table.');window.location='index.php';</script>";
+  }else{
+  	$check = "SELECT * FROM folder2 WHERE type = 'A' AND year = '$year'";
+  	$get = mysqli_query($mysqli, $check);
+  	$fetch = mysqli_fetch_assoc($get);
+  	if (!$fetch) {
+  		echo "<script>alert('Create a School Year for this batch in Employees Yearbook Database.');window.location='index.php';</script>";
+  	}
+  	else{
+  	$my = "INSERT INTO tbl_eybook (eid, elname, efname, emname, work_status, department, employee_year) VALUES ('$id', '$ln', '$fn', '$mn', '$role', '$dep', '$year')";
+  	$result = mysqli_query($mysqli, $my);
+  	echo mysqli_error($mysqli);
 
-}
+  	echo "<script>alert('Added to Yearbook Successfully.');window.location='index.php';</script>";
+    }
+  }
 }
 
 if (isset($_POST['cancel'])) {
